2de01a89ef2730998970e1fc98e3df5d92f97e9f
glitch452 <894128+glitch452@users.noreply.github.com>
2019-10-25 15:56:21 -0400

    fixed and added test code

--- good.py
+++ good.py
@@ -1,9 +1,26 @@
 #(m, n)
-import random
+import copy, random
+
+def matrix_bad_but_accurate(lst):
+    if len(lst) < 2:
+        return 0
+    best = -1
+    for i in range(1, len(lst)):
+        curr = lst[i]
+        prev = lst[i-1]
+        cost = prev[0] * curr[0] * curr[1]
+        cpy = copy.deepcopy(lst)
+        cpy[i] = (prev[0], curr[1])
+        del cpy[i-1]
+        cost = cost + matrix_bad_but_accurate(cpy)
+        if best == -1 or cost < best:
+            best = cost
+    return best
 
 
 def test_matrix(lst):
-    if len(lst) == 1:
+    lst = copy.deepcopy(lst)
+    if len(lst) < 2:
         return 0
 
     costs = []
@@ -20,10 +37,6 @@ def test_matrix(lst):
     return costs[index][0] + test_matrix(lst)
 
 
-lst = [(1,2), (2,3), (3,4), (4,5)]
-
-print(test_matrix(lst))
-
 def gen_test_cases(size, min_int, max_int):
     previous = random.randint(min_int, max_int)
     matrices = []
@@ -33,4 +46,17 @@ def gen_test_cases(size, min_int, max_int):
         previous = next
     return matrices
 
-print(gen_test_cases(10, 1, 10))
+def test():
+    test_cases = []
+    for i in range(50000):
+        test = gen_test_cases(5, 1, 500)
+        test_cases.append(test)
+    for case in test_cases:
+        correct = matrix_bad_but_accurate(case)
+        test_ans = test_matrix(case)
+        if correct != test_ans:
+            print(case)
+            print("Correct Answer: {correct} \n False Answer: {incorrect}".format(correct=correct, incorrect=test_ans))
+            return False
+
+test()
